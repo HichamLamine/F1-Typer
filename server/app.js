5b2ff9f5296e9712a19203623e994446a299a238
HichamLamine <hichamlamine29@gmail.com>
2024-10-19 20:24:04 +0100

    working on authentication

--- server/app.js
+++ server/app.js
@@ -1,2 +1,12 @@
 import http from 'http';
 import fs from 'fs';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/html'});
+    fs.createReadStream('index.html').pipe(res);
+});
+
+server.listen(port);
+console.log(`server started on port ${port}`);
